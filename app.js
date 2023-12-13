0e218b56252df1b90161298ee746c1649c13c8fe
“Sebastian <impactotenisok@gmail.com>
2023-12-13 08:16:10 -0300

    Se quito dependencia  fetch

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const path = require("path");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
-const fetch = require("node-fetch");
 
 const app = express();
 const port = process.env.PORT || 3000;
